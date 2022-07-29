99f88b1fa174400157cb1bbc0976844e733b621a
Rihan Mohammed <rihanmohammed26@gmail.com>
2022-07-29 11:34:32 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as registerServiceWorker from './registerServiceWorker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const startApp = () => {
+	ReactDOM.render(
+		(
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		),
+		document.getElementById('root'));
+	registerServiceWorker();
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if (window.cordova) {
+	document.addEventListener('deviceready', startApp, false);
+} else {
+	startApp();
+}
